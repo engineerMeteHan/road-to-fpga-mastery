a27552e6ec740ce75bbda4c15b6805723b045440
engineerMeteHan <metehangencereem@example.com>
2025-10-25 23:13:31 +0300

    updated and_gate file

--- 01_Basics/logic_gates/and_gate/and_gate.srcs/sources_1/new/and_gate.vhd
+++ 01_Basics/logic_gates/and_gate/and_gate.srcs/sources_1/new/and_gate.vhd
@@ -32,12 +32,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity and_gate is
---  Port ( );
+ Port (
+    a : in std_logic;
+    b : in std_logic;
+    c: out std_logic
+  );
 end and_gate;
 
 architecture Behavioral of and_gate is
 
 begin
 
-
+    c <= a and b;
+    
 end Behavioral;
